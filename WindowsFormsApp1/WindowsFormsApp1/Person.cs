b49ce7bd1a975ea10db240e6b7392570068fe2f0
Cajee7 <MuhammedCajee29@gmail.com>
2023-08-31 11:57:15 +0200

    I created the person class. P.s: Youll smell

---
+++ WindowsFormsApp1/WindowsFormsApp1/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Person
+    {
+        // Properties
+        public string Name { get; private set; }
+        public string Contact { get; private set; }
+        public DateTime Birthday { get; private set; }
+
+        // Constructors
+        public Person(string name, string contact, DateTime birthday)
+        {
+            Name = name;
+            Contact = contact;
+            Birthday = birthday;
+        }
+
+        // Methods to set properties
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public void SetContact(string contact)
+        {
+            Contact = contact;
+        }
+
+        public void SetBirthday(DateTime birthday)
+        {
+            Birthday = birthday;
+        }
+
+        // Methods to get properties
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetContact()
+        {
+            return Contact;
+        }
+
+        public DateTime GetBirthday()
+        {
+            return Birthday;
+        }
+
+        // Override ToString() method
+        public override string ToString()
+        {
+            return $"Name: {Name}\nContact: {Contact}\nBirthday: {Birthday.ToShortDateString()}";
+        }
+    }
+}
+
