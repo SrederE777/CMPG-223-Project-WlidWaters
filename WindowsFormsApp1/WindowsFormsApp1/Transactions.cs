38de599d12c346451a736c295d78ae4530ecd26e
zahrakara03 <143691458+zahrakara03@users.noreply.github.com>
2023-08-31 13:49:29 +0200

    Created transactions

---
+++ WindowsFormsApp1/WindowsFormsApp1/Transactions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Transactions
+    {
+        private string customer;
+        private DateTime transactionDate;
+        private TimeSpan transactionTime;
+        private int ticketAmount;
+        private double transactionAmount;
+
+        public void SetCustomer(string customer)
+        {
+            this.customer = customer;
+        }
+
+        public void SetTransaction_Date(DateTime transactionDate)
+        {
+            this.transactionDate = transactionDate;
+        }
+
+        public void SetTransaction_Time(TimeSpan transactionTime)
+        {
+            this.transactionTime = transactionTime;
+        }
+
+        public void SetTicket_Amount(int ticketAmount)
+        {
+            this.ticketAmount = ticketAmount;
+        }
+
+        public void SetTransaction_Amount(double transactionAmount)
+        {
+            this.transactionAmount = transactionAmount;
+        }
+
+        public string GetCustomer()
+        {
+            return customer;
+        }
+
+        public DateTime GetTransaction_Date()
+        {
+            return transactionDate;
+        }
+
+        public TimeSpan GetTransaction_Time()
+        {
+            return transactionTime;
+        }
+
+        public int GetTicket_Amount()
+        {
+            return ticketAmount;
+        }
+
+        public double GetTransaction_Amount()
+        {
+            return transactionAmount;
+        }
+
+        public void SellTicket()
+        {
+            // Implement ticket selling logic here
+        }
+
+        public double CalCost()
+        {
+            // Implement cost calculation logic here
+            return 0.0;
+        }
+
+        public override string ToString()
+        {
+            return $"Customer: {customer}\n" +
+                   $"Transaction Date: {transactionDate.ToString("yyyy-MM-dd")}\n" +
+                   $"Transaction Time: {transactionTime.ToString(@"hh\:mm")}\n" +
+                   $"Ticket Amount: {ticketAmount}\n" +
+                   $"Transaction Amount: {transactionAmount}";
+        }
+    }
+}
