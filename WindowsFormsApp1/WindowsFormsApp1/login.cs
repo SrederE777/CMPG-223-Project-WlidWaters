60bd56cecc22f3d84b3f61b3c7f9aa4a3fe539dc
zahrakara03 <143691458+zahrakara03@users.noreply.github.com>
2023-08-31 15:39:22 +0200

    Made login class

--- WindowsFormsApp1/WindowsFormsApp1/login.cs
+++ WindowsFormsApp1/WindowsFormsApp1/login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,36 @@ namespace WindowsFormsApp1
 {
     class login
     {
+        private string connectionString; // Database connection string
+
+        public login(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+
+            // Assuming you have a database table named "Users" with columns "Username" and "Password"
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@Password", password);
+
+                    int matchingUserCount = (int)command.ExecuteScalar();
+
+                    return matchingUserCount > 0;
+                }
+            }
+        }
     }
 }
