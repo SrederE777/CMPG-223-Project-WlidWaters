2250c2193b584355422e7c849ffdad0d8c918593
zahrakara03 <143691458+zahrakara03@users.noreply.github.com>
2023-08-31 12:42:47 +0200

    updated code

--- WindowsFormsApp1/WindowsFormsApp1/Customer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Customer.cs
@@ -8,5 +8,22 @@ namespace WindowsFormsApp1
 {
     internal class Customer
     {
+        private String email;
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+
+        public String getEmail()
+        {
+            return email;
+        }
+
+        @Override
+    public String toString()
+        {
+            return "Email: " + email;
+        }
     }
 }
